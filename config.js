093fbc78f7773d01acc4a99423379ee759e9eda5
nataliyasniguryak28-sys <nataliyasniguryak28@gmail.com>
2026-09-07 20:16:57 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,3 @@
+// Вставь сюда адрес своей Firebase Realtime Database.
+// Пример: https://my-project-default-rtdb.europe-west1.firebasedatabase.app
+window.DATABASE_URL = "PASTE_YOUR_FIREBASE_DATABASE_URL_HERE";
